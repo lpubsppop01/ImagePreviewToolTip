fed9bbae72aebf676cdc65f87d57a43991c075dc
lpubsppop01 <lpubsppop01@gmail.com>
2017-04-18 23:42:51 +0900

    add pre/post build scripts

---
+++ ImagePreviewToolTipVSIX/_Build/CopyBuildResult.ps1
@@ -0,0 +1,11 @@
+﻿if ($args.Length -gt 0) {
+    $configulationName = $args[0]
+    if ($configulationName -ine "Publish") {
+        return
+    }
+}
+
+$scriptDirPath = Split-Path $MyInvocation.MyCommand.Path -Parent
+$srcFilePath = Join-Path $scriptDirPath "..\bin\Publish\lpubsppop01.ImagePreviewToolTipVSIX.vsix"
+$destFilePath = Join-Path $scriptDirPath "..\..\ImagePreviewToolTipVSIX.vsix"
+copy $srcFilePath $destFilePath
---
+++ ImagePreviewToolTipVSIX/_Build/IncrementBuildVersion.ps1
@@ -0,0 +1,19 @@
+﻿if ($args.Length -gt 0) {
+    $configulationName = $args[0]
+    if ($configulationName -ine "Publish") {
+        return
+    }
+}
+
+$scriptDirPath = Split-Path $MyInvocation.MyCommand.Path -Parent
+$manifestFilePath = Join-Path $scriptDirPath "..\source.extension.vsixmanifest"
+$manifestXML = [xml](Get-Content $manifestFilePath)
+$identityElement = $manifestXML.SelectSingleNode("//*[local-name(.) = 'Identity']")
+
+$oldVersion = $identityElement.Version
+$v = [version]$identityElement.Version
+$newVersion = $v.Major.ToString() + "." + $v.Minor.ToString() + "." + ($v.Build + 1).ToString()
+Write-Host "Version: $oldVersion -> $newVersion"
+
+$identityElement.Version = $newVersion
+$manifestXML.Save($manifestFilePath)
